7cbc509cc6a89c518effb1d90acb948265aab629
Huanhuan Sun <husun@microsoft.com>
2017-06-22 17:39:46 -0700

    Add project files.

---
+++ SerilogAsyncDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using Serilog;
+using Serilog.Context;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class SerilogAsyncDemo
+{
+    static void Main()
+    {
+        Init();
+
+        TestLogAsync().Wait();
+    }
+
+    private static async Task TestLogAsync()
+    {
+        Console.WriteLine("TestLogAsync is on thread: " + Thread.CurrentThread.ManagedThreadId);
+        using (LogContext.PushProperty("OperationId", 120))
+        {
+            Log.Information("TestLogAsync is on thread: " + Thread.CurrentThread.ManagedThreadId);
+
+            await TestLog2Async();
+        }
+    }
+
+    private static Task TestLog2Async()
+    {
+        return Task.Run(() =>
+        {
+            Console.WriteLine("TestLog2Async  is on thread: " + Thread.CurrentThread.ManagedThreadId);
+            Log.Logger.Information("TestLog2Async  is on thread: " + Thread.CurrentThread.ManagedThreadId);
+        });
+    }
+
+    private static void Init()
+    {
+        Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Debug()
+        .Enrich.WithProperty("MachineName", Environment.MachineName)
+        .Enrich.FromLogContext()
+        .WriteTo.Console(
+            outputTemplate: "[{Timestamp:hh:mm:ss} {Level:u3}] {Message,-30:lj} {Properties:j}{NewLine}{Exception}")
+        .CreateLogger();
+    }
+
+}
+
